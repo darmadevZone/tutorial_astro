de627c9ec36b8fedb3ed9ef8d90d8aef2e6e8b62
darmadevZone <darmadevdone@gmail.com>
2023-01-19 16:53:37 +0900

    finished tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
+import react from "@astrojs/react";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), svelte()]
+});
